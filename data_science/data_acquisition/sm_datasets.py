07f9d69ba745a05a590d97e4f5efcba92785ab1f
Chanhee Lee <qjzx6428@gmail.com>
2023-02-26 02:34:27 +0900

    data acquisition for "HistData.Guerry" (#3)
    ### 1. 데이터 다운받기
    data_science 폴더 내에서
    ```python
    from data_acquisition import sm_datasets
    
    sm_datasets.download("HistData.Guerry")
    ```
    
    ### 2. 데이터 읽어오기
    data_science 폴더 내에서
    ```python
    from data_acquisition import sm_datasets
    
    df_guerry = sm_datasets.read("HistData.Guerry")
    ```
    `df_guerry`는 pandas DataFrame 형태

---
+++ data_science/data_acquisition/sm_datasets.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import statsmodels.api as sm
+import pandas as pd
+
+# Default paths
+ROOT = Path(__file__).parents[2]
+DEFAULT_PATH = ROOT / "datasets"
+
+rdata_args = {"HistData.Guerry": ("Guerry", "HistData")}
+rdata_path = {"HistData.Guerry": DEFAULT_PATH / "guerry.csv"}
+
+
+def download(dataset_name, target_path=""):
+    target_path = target_path if target_path else rdata_path[dataset_name]
+    args = rdata_args[dataset_name]
+
+    # Download data here
+    df = sm.datasets.get_rdataset(*args).data
+
+    # Save as csv
+    df.to_csv(target_path)
+
+    return target_path
+
+
+def read(dataset_name, src_path=""):
+    src_path = src_path if src_path else rdata_path[dataset_name]
+    args = rdata_args[dataset_name]
+
+    df = pd.read_csv(src_path, index_col=0)
+    return df
