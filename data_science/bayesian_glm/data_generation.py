dafe9e5d2374fbb0e6b50d64be2a368074835499
Chanhee Lee <qjzx6428@gmail.com>
2023-03-04 17:32:18 +0900

    reformat data_generation.py with black

--- data_science/bayesian_glm/data_generation.py
+++ data_science/bayesian_glm/data_generation.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def define_rng(random_seed=8927):
     return np.random.default_rng(random_seed)
 
+
 rng = define_rng()
+
 
 def sample_xy(intercept_gen, slope_gen, sample_size=100):
     x = np.linspace(0, 1, sample_size)
@@ -18,7 +21,9 @@ def sample_xy(intercept_gen, slope_gen, sample_size=100):
 
 def plot_xy(x, y, model):
     fig = plt.figure(figsize=(7, 7))
-    ax = fig.add_subplot(111, xlabel="x", ylabel="y", title="Generated data and unerlying model")
+    ax = fig.add_subplot(
+        111, xlabel="x", ylabel="y", title="Generated data and unerlying model"
+    )
     ax.plot(x, y, "x", label="sampled data")
     ax.plot(x, model, label="true regression line", lw=2.0)
     plt.legend(loc=0)
