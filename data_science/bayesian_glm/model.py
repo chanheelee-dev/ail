f12583d48250c44f4f63ca191bdfebf4f05bd0e3
Chanhee Lee <qjzx6428@gmail.com>
2023-03-11 14:15:59 +0900

    add glm example

---
+++ data_science/bayesian_glm/model.py
@@ -0,0 +1,38 @@
+import pymc as pm
+import arviz as az
+import xarray as xr
+import matplotlib.pyplot as plt
+
+def define_model(x, y):
+    with pm.Model() as model:
+        # Define priors
+        sigma = pm.HalfCauchy("sigma", beta=10)
+        intercept = pm.Normal("intercept", 0, sigma=20)
+        slope = pm.Normal("slope", 0, sigma=20)
+
+        # Define likelihood
+        likelihood = pm.Normal("y", mu=intercept + slope * x, sigma=sigma, observed=y)
+
+    return model
+
+
+def inference(model, sample_size=3000):
+    with model:
+        idata = pm.sample(sample_size)
+    return model, idata
+
+
+if __name__ == "__main__":
+    from data_generation import sample_xy
+
+    x, y, line = sample_xy(intercept_gen=3, slope_gen=2)
+    model = define_model(x, y)
+    model, idata = inference(model)
+
+    # TODO: Analysis for glm result
+    fig, ax = plt.subplots(figsize=(7, 7))
+    # idata.posterior["y_model"] = idata.posterior["intercept"] + idata.posterior["slope"] * xr.DataArray(x)
+    # az.plot_lm(idata=idata, y="y", num_samples=100, axes=ax, y_model="y_model")
+    # ax.set_title("Posterior predictive regression lines")
+    # ax.set_xlabel("x")
+    # fig.show()
